9e20dab587a4e6ba2c8c36dcd5bc202b6eda6ade
hfq-lixiangyang <lixiangyang@huizhaofang.com>
2017-12-14 00:44:46 +0800

    change: mysql

--- database/index.js
+++ database/index.js
@@ -4,5 +4,17 @@ const pool = mysql.createPool({
     host    : 'localhost',
     user    : 'root',
     password: '001208',
-    database: 'fed'
+    database: 'fed',
+    port    : 8888
 })
+
+/*pool.getConnection(function(err, connection){
+    if(err) throw err
+    connection.query('SELECT 1 + 1 AS solution', function(err, resu, fields){
+        console.log(connection.escapeId('select'))
+        connection.destroy()
+    })
+})*/
+
+console.log(mysql.format('SELECT ? FROM ? WHERE age = ?', ['username', mysql.raw('CURDATE()'), 22]))
+console.log(mysql.raw('CURDATE()'))
